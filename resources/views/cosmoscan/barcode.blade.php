dde82470048df9e4f55781d375b244562b164521
cosmegeek <nujabes1025@gmail.com>
2020-01-13 18:14:43 +0900

    “コミットテスト”

--- resources/views/cosmoscan/barcode.blade.php
+++ resources/views/cosmoscan/barcode.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.js"></script>
+<script src="http://cosmoscan.sakura.ne.jp/resources/assets/js/quaggaJS-master/dist/quagga.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
 
 
